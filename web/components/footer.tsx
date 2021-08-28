0918861ab81fe97c6d5fa93a4190593cf26e0d67
J Shamsul Bahri (jibone)) <jibone@gmail.com>
2021-08-28 17:59:12 +0800

    Update packages and notes

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -13,7 +13,8 @@ const Footer = () => {
         <div className="py-28 flex flex-col">
           <div className="w-full h-24" style={bgImg} />
           <div className="text-xs mt-6">
-            Copyright &copy; haithal.com. All Rights Reserved.
+            Copyright &copy; <a href="https://haithal.com">haithal.com</a>. All
+            Rights Reserved.
           </div>
         </div>
       </Container>
